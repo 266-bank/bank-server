a46ffb4fee84dbd9c7bfc9b76432770f16372db2
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2021-04-20 14:52:50 -0700

    set up

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-var express = require("express");
+const express = require("express");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
