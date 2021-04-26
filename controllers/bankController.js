f30bf21001eb00b314e14592b8da3a834e5d0660
TssT96 <42284547+TssT96@users.noreply.github.com>
2021-04-25 23:58:47 -0700

    get api update

--- controllers/bankController.js
+++ controllers/bankController.js
@@ -2,20 +2,20 @@ const Repo = require("../repositories");
 
 module.exports = {
     userDeposit: function(req, res) {
-        Repo.Bank.userRepoDeposit(req.body.username, req.body.amount)
+        Repo.Bank.userRepoDeposit(req)
         .then(dbModel => res.json(dbModel))
         // unsure about the status code
         .catch(err => res.status(500).json(err));
     },
 
     userWithdrawal: function(req, res) {
-        Repo.Bank.userRepoWithdrawal(req.body.username, req.body.amount)
+        Repo.Bank.userRepoWithdrawal(req)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(500).json(err));
     },
 
     userGetBalance: function(req, res) {
-        Repo.Bank.userRepoGetBalance(req.body.username)
+        Repo.Bank.userRepoGetBalance(req)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(500).json(err));
     }
--- repositories/bankRepository.js
+++ repositories/bankRepository.js
@@ -2,20 +2,20 @@ const DAOLayer = require("../DAOlayer");
 
 module.exports = {
     userRepoDeposit: function(req, res) {
-        DAOLayer.BankDAO.userDeposit(req)
+        DAOLayer.BankDAO.userDeposit(req.body.username, req.body.amount)
         .then(dbModel => res.json(dbModel))
         // not too sure about the status code, can change later
         .catch(err => res.status(500).json(err));
     },
 
     userRepoWithdrawal: function(req, res) {
-        DAOLayer.BankDAO.userWithdrawal(req)
+        DAOLayer.BankDAO.userWithdrawal(req.body.username, req.body.amount)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(500).json(err));
     },
 
     userRepoGetBalance: function(req, res) {
-        DAOLayer.BankDAO.userGetBalance(req)
+        DAOLayer.BankDAO.userGetBalance(req.body.username)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(500).json(err));
     }
