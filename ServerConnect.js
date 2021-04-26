f3d2762ea132b085fc7b5931880143f02f5c407a
andyheko <haoen.ko@gmail.com>
2021-04-25 23:57:31 -0700

    connecting to heroku db

--- ServerConnect.js
+++ ServerConnect.js
@@ -3,12 +3,24 @@ const db = require("pg");
 const { Client } = require('pg');
 
 exports.client = new Client({
-  // connectionString: 'http://127.0.0.1:11562/?key=a7422cd2-e52c-4cf5-a982-8d90f787f501', //
-  connectionString: process.env.DATABASE_URL,
+  user: 'ykpuhjiqwckqby',
+  host: 'ec2-52-45-73-150.compute-1.amazonaws.com',
+  database: 'd54n3cf1sf38rj',
+  password: '5127f9f796e952299419142c4f0211d5c82e9ec2e8fc9efba29f73f233c3ef8b',
+  port: 5432,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  connectionString: 'postgres://ykpuhjiqwckqby:5127f9f796e952299419142c4f0211d5c82e9ec2e8fc9efba29f73f233c3ef8b@ec2-52-45-73-150.compute-1.amazonaws.com:5432/d54n3cf1sf38rj'
 });
+
+// exports.client = new Client({
+//   // connectionString: 'http://127.0.0.1:11562/?key=a7422cd2-e52c-4cf5-a982-8d90f787f501', //
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
 
 // exports.client = new Client({
