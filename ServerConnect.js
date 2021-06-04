8c8270d831b639c8438086def23227d8b17eb788
andyheko <haoen.ko@gmail.com>
2021-06-04 13:42:11 -0700

    Update ServerConnect.js

--- ServerConnect.js
+++ ServerConnect.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg');
 
 exports.client = new Client({
-  // connectionString: 'http://127.0.0.1:11562/?key=a7422cd2-e52c-4cf5-a982-8d90f787f501', //
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
